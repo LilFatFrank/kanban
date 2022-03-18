984ec807ec1ee78a5b72d0b56b255ff0b5e0f754
LilFatFrank <karan.parsnani@ymail.com>
2022-03-18 14:45:09 +0530

    removing structure to create an empty view on load

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,22 +1,3 @@
-export const data = {
-  columns: {
-    "column-1": {
-      id: "column-1",
-      title: "Test Column",
-      cards: [
-        {
-          id: "1",
-          title: "Test Card",
-          description: "Test Description",
-          status: "open",
-          createdDate: new Date().toLocaleDateString()
-        }
-      ]
-    }
-  },
-  columnIds: ["column-1"]
-};
-
 export const archivedStorage = JSON.parse(
   localStorage.getItem("archivedDataKanban")
 );
@@ -26,8 +7,7 @@ export const initialState = () => {
   if (dataStorage) {
     return dataStorage;
   } else {
-    localStorage.setItem("dataKanban", JSON.stringify(data));
-    return data;
+    return {};
   }
 };
 
